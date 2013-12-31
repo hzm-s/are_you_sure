e03777a3f43f5095cc6834b3485be551a9d96b8b
haazime <h4zime@gmail.com>
2013-12-31 16:30:49 +0900

    refactor confirm spec

--- spec/feature/confirm_spec.rb
+++ spec/feature/confirm_spec.rb
@@ -1,22 +1,5 @@
 require 'spec_helper'
 
-class LensSpec < Struct.new(:registered_at, :mfr, :mount, :name, :mm, :f, :close_up, :note)
-
-  def close_up_state
-    close_up ? 'available' : 'unavailable'
-  end
-
-  def registered_datetime
-    registered_at.strftime("%Y-%m-%d %H:%M")
-  end
-
-  def registered_year;  registered_at.year;  end
-  def registered_month; registered_at.month; end
-  def registered_day;   registered_at.day;   end
-  def registered_hour;  "%02d" % registered_at.hour;  end
-  def registered_min;   "%02d" % registered_at.min;   end
-end
-
 feature 'confirm add lens' do
   background do
     visit 'lenses/new'
@@ -35,7 +18,7 @@ feature 'confirm add lens' do
   end
 
   let(:lens) do
-    LensSpec.new(
+    LensSpecification.new(
       Time.current,
       MFR.find_by(name: 'Leica'),
       Mount.find_by(name: 'Leica M'),
