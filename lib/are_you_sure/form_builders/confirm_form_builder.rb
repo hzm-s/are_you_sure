42d3f6ac4e531fcac4734903bf67212a5825eaf0
Kazuhiro Homma <kazu.homma@gmail.com>
2014-03-27 15:52:22 +0900

    passwordの処理も他のメソッドと抽象度を合わせた

--- lib/are_you_sure/form_builders/confirm_form_builder.rb
+++ lib/are_you_sure/form_builders/confirm_form_builder.rb
@@ -47,7 +47,7 @@ module AreYouSure
       when /text_area/
         text_area_field_value(field_value, *options)
       when /password/
-        '********'
+        password_field_value(field_value, *options)
       else
         field_value
       end
@@ -73,6 +73,10 @@ module AreYouSure
 
     def text_area_field_value(text, *options)
       text.gsub(/\r\n|\n|\r/, '<br />').html_safe
+    end
+
+    def password_field_value(password, *options)
+      '********'
     end
   end
 end
